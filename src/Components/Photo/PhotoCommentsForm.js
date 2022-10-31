0fb6a26992da4ecdaa8b7e110124d2e31b156915
patrick2m <pmm12@hotmail.com>
2022-10-31 17:57:33 -0300

    822 Comentário Post

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error'
+import { COMMENT_POST } from '../../Api';
 
-const PhotoCommentsForm = () => {
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState('')
+  const {request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, {comment});
+    const { response, json } = await request(url, options);
+    if(response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
   return (
-    <div>PhotoCommentsForm</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({target}) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
 
 export default PhotoCommentsForm
