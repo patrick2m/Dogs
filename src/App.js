7bf4cc512b013703dc4cbeb50d481a6605375e6e
patrick2m <pmm12@hotmail.com>
2022-11-16 13:49:51 -0300

    829 Perfil Usuário

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Photo from './Components/Photo/Photo';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User'
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import UserProfile from './Components/User/UserProfile';
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
                 </ProtectedRoute>}
               />
               <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
