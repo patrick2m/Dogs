654c5205100cafb599448cdb16bb801fccd82bc7
Patrick PC de Casa <pmm12@hotmail.com>
2022-11-20 10:43:58 -0300

    corrigido component para direcionamento correto do link para homepage

--- src/App.js
+++ src/App.js
@@ -20,18 +20,12 @@ const App = () => {
           <Header />
           <main className='AppBody'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="dogs" element={<Home />} />
               <Route path="login/*" element={<Login />} />
-              <Route
-                path="conta/*"
-                element={
-                  <ProtectedRoute>
-                    <User />
-                  </ProtectedRoute>}
-                />
-                <Route path="foto/:id" element={<Photo />} />
-                <Route path="perfil/:user" element={<UserProfile />} />
-                <Route path="*" element={<NotFound />} />
+              <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>}/>
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
           <Footer />
